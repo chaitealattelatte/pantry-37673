58fac570b0af8faab138969881df1093fdcedb6d
chaitealattelatte <chaitealattelatte@gmail.com>
2022-07-10 12:02:57 +0900

    READMEの記述追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    family_name           {'test'}
+    first_name            {'test'}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+       @user.email = ''
+       @user.valid?
+       expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    
+  end
+end
+
+
+# nickname              {'test'}
+# email                 {'test@example'}
+# password              {'000000'}
+# password_confirmation {password}
+# family_name           {'test'}
+# first_name            {'test'}
